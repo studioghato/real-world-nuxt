3d10098ba0e37f86d975721330bf4331a408c5f8
studioghato <sg@test.com>
2019-10-01 19:54:33 +0200

    SEO with built-in vue-meta library

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,17 @@
     <h1>Events</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+      /* Since we are not defining a meta property with its own
+       * hid: 'description', this page will inherit the meta
+       * description set on the "default.vue" layout.
+       */
+    }
+  }
+}
+</script>
