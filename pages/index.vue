97f023b09d31239484d62823587f72b769ead260
studioghato <sg@test.com>
2019-10-02 16:54:09 +0200

    API calls with Axios and Error handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event Listing'
@@ -14,6 +25,21 @@ export default {
        * description set on the "default.vue" layout.
        */
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
   }
 }
 </script>
